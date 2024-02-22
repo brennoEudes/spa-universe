39c2cc63ed50d1caed75acd7a544719cc1e3168d
“Brenno <brennoeudes@gmail.com>
2024-02-22 17:21:07 -0300

    404 page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const router = new Router();
 router.add("/", "pages/home.html");
 router.add("/universe", "pages/universe.html");
 router.add("/exploration", "pages/exploration.html");
+router.add("404", "pages/404.html");
 
 router.handle(); // executa a fcn handle também após a leitura do script.
 
